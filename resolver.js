d4147ed2bb5266b7a4333e581efd745f5c8d49b2
Sarthak Rohatgi <infinitybusiness726@gmail.com>
2021-02-23 19:05:54 +0530

    Resolver Functions Added
    Resolver Functions named, info(), getUserByEmailId(), getAllUsers() and createNewUser() are written and working fine

--- resolver.js
+++ resolver.js
@@ -1,10 +1,34 @@
+const { nanoid } = require("nanoid");
 
 const resolvers = {
   Query: {
-    info: () => 'This is the GraphQL API for the used books app.',
-    user: () => users[0],
-    // feed: () => links,
-    books: () => books[2]
+    info: () => "This is the GraphQL API for the used books app.",
+
+    getUserByEmailId: (_parent, args, context, _info) =>
+      context.prisma.user.findUnique({
+        where: {
+          emailId: args.emailId,
+        },
+      }),
+    
+    getAllUsers: async (_parent, _args, context, _info) =>
+      context.prisma.user.findMany(),
+  },
+  // All the mutation's resolver functions are described below
+  Mutation: {
+    createNewUser: (_parent, args, context, _info) => {
+      const newUser = context.prisma.user.create({
+        data: {
+          id: nanoid(12),
+          uuid: args.uuid,
+          firstName: args.firstName,
+          lastName: args.lastName,
+          emailId: args.emailId,
+          phoneNumber: args.phoneNumber,
+        },
+      });
+      return newUser;
+    },
   },
 };
 
